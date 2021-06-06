3fde6b5fcac73c07300c0f559b99d22ce4fa628a
DavidTK1198 <ddanielb09@gmail.com>
2021-06-05 20:57:46 -0600

    Pelicula

--- web/js/usuarios.js
+++ web/js/usuarios.js
@@ -72,8 +72,8 @@ function login() {
     data: JSON.stringify(data),
     contentType: "application/json"
   }).then((response) => {
-  
-      location.href="/Cinema/web/presentation/administrador.html";
+
+    location.href = "/Cinema/web/presentation/administrador.html";
   },
     (error) => {
       console.log(error);
@@ -82,11 +82,69 @@ function login() {
 function registro() {
 
 }
+function draw_movie() {
+  $("#cambiar").hide();
+  $("#barra").hide();
+  try {
+    let bandera = !!document.getElementById("change");
+    throw bandera
+  } catch (error) {
+    if (!error) {
+     var contenedor = document.getElementById("content");
+     var fila=document.createElement("div");
+     fila.classList.add("text-black", "row", "justify-content-center");
+     contenedor.appendChild(fila);
+      contenedor.classList.add("text-black", "justify-content-center");
+     var d = document.createElement("div");
+      d.id = 'change';
+      d.classList.add("col", "col-sm-8", "col-md-4", "col-xl-4","d-flex");
+      d.innerHTML = (`<div class="d-flex">
+    <form  class="text-black mt-5" name="form"  method="post" > 
+    <h3>Registrar Pelicula</h2>
+    <div class="mb-2">
+        <div class="font-weight-bold mb-0">Nombre</div>
+        <div class="campo"><input class="form-control" placeholder="Nombre de la Pelicula" type="text" name="cedulaFld"></div>
+    </div>
+    <div class="mb-2">
+        <div class="font-weight-bold mb-0">Nombre</div>
+        <div class="campo"><input class="form-control" placeholder="Poner estado tarea" type="text" name="NombreFld"></div>
+    </div>
+    <div class="mb-2">
+    <div class="flex-container">
+        <label>Estado de la Pelicula</label>
+        <div id="flex">
+            <div class="flex-item">
+                <input type="radio" name="oferta" value="1" checked>Pelicula en Cartelera
+            </div>
+        </div>
 
-function loaded() {
-  $("#loginButton").click(login);
-  $("#RegButton").click(registro);
+        <div id="flex">
+            <div class="flex-item">
+                <input type="radio" name="oferta" value="0">Proximamente
+            </div>
+        </div>
+    </div>
+</div>
+    <button class="btn btn-success btn-lg btn-block">Registrar</button>
+</form>
+</div>`
+);
+      fila.appendChild(d);
+    }
+  }
 }
-$(loaded);
+
+function draw_home(){
+  $("#cambiar").show();
+  $("#barra").show();
+  $("#change").remove();
+}
+  function loaded() {
+    $("#loginButton").click(login);
+    $("#RegButton").click(registro);
+    $("#pelicula").click(draw_movie);
+    $("#home").click(draw_home);
+  }
+  $(loaded);
 
 
